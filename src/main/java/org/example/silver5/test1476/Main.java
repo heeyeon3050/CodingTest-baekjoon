07cfe62adc35fff4b89748e9b8130bfd52999600
heeyeon3050 <kho200a@naver.com>
2023-05-18 00:42:22 +0900

    test1476

---
+++ src/main/java/org/example/silver5/test1476/Main.java
@@ -0,0 +1,43 @@
+package org.example.silver5.test1476;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int E = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int e=0, s=0, m=0, year=0;
+        while(true){
+            e++;
+            s++;
+            m++;
+            year++;
+
+            if(e == 16){
+                e = 1;
+            }
+
+            if(s == 29){
+                s = 1;
+            }
+
+            if(m == 20){
+                m = 1;
+            }
+
+            if(e == E && s == S && m == M){
+                break;
+            }
+        }
+
+        System.out.println(year);
+    }
+}
