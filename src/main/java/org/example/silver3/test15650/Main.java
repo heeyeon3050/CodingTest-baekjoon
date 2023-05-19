5960d9f8588d3db8fb7f49e0630cb60c7df2d31d
heeyeon3050 <kho200a@naver.com>
2023-05-19 13:45:09 +0900

    test15650

---
+++ src/main/java/org/example/silver3/test15650/Main.java
@@ -0,0 +1,40 @@
+package org.example.silver3.test15650;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int[] arr;
+    static boolean[] visit;
+    static int n, m;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        visit = new boolean[n+1];
+
+        dfs(0, arr, 1);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int L, int[] arr, int start){
+        if(L == m){
+            for(int i=0; i<m; i++){
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=start; i<=n; i++) {
+            arr[L] = i;
+            dfs(L + 1, arr, i+1);
+        }
+    }
+}
