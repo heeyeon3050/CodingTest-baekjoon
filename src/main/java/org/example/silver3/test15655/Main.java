b99c828764351327d33f9f1c6ba9e2c4ed70cc2a
heeyeon3050 <kho200a@naver.com>
2023-05-19 15:54:56 +0900

    test15655

---
+++ src/main/java/org/example/silver3/test15655/Main.java
@@ -0,0 +1,51 @@
+package org.example.silver3.test15655;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int[] arr;
+    static boolean[] visit;
+    static int n, m;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        visit = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0, new int[m], 0);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int L, int[] num, int start){
+        if(L == m) {
+            for(int i=0; i<m; i++){
+                sb.append(num[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=start; i<n; i++){
+            num[L] = arr[i];
+            dfs(L+1, num, i+1);
+        }
+    }
+}
